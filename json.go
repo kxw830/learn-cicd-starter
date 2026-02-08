de3b28af886b76281022584120b6803d352b0a0d
ubuntu <ajcool.aj2@gmail.com>
2026-02-07 19:35:36 -0600

    add: included vulnerabilities to verify gosec

--- json.go
+++ json.go
@@ -30,7 +30,5 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 		return
 	}
 	w.WriteHeader(code)
-	if _, err := w.Write(dat); err != nil {
-		log.Printf("Failed to write response: %v", err)
-	}
+	w.Write(dat)
 }
